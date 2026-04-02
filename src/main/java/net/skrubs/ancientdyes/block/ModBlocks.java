8efd026e2f5d2104f4943b9bff2ad09fceab993f
SkrubsTheNeko <Ajawsome03@gmail.com>
2026-04-02 16:13:42 -0700

    Work in Progress

---
+++ src/main/java/net/skrubs/ancientdyes/block/ModBlocks.java
@@ -0,0 +1,182 @@
+package net.skrubs.ancientdyes.block;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.block.*;
+import net.minecraft.block.enums.NoteBlockInstrument;
+import net.minecraft.block.piston.PistonBehavior;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.DyeColor;
+import net.minecraft.util.Identifier;
+import net.skrubs.ancientdyes.AncientDyes;
+
+
+public class ModBlocks {
+    public static final Block CHARTREUSE_CONCRETE = registerBlock(
+            "chartreuse_concrete", new Block(AbstractBlock.Settings.create().mapColor(DyeColor.LIME).instrument(NoteBlockInstrument.BASEDRUM).requiresTool().strength(1.8F))
+    );
+    public static final Block CHARTREUSE_CONCRETE_POWDER = registerBlock("chartreuse_concrete_powder",
+            new ConcretePowderBlock(
+                    CHARTREUSE_CONCRETE, AbstractBlock.Settings.create().mapColor(DyeColor.LIME).instrument(NoteBlockInstrument.SNARE).strength(0.5F).sounds(BlockSoundGroup.SAND)
+            ));
+    public static final Block ROSE_CONCRETE = registerBlock(
+            "rose_concrete", new Block(AbstractBlock.Settings.create().mapColor(DyeColor.MAGENTA).instrument(NoteBlockInstrument.BASEDRUM).requiresTool().strength(1.8F))
+    );
+    public static final Block ROSE_CONCRETE_POWDER = registerBlock("rose_concrete_powder",
+            new ConcretePowderBlock(
+                    ROSE_CONCRETE, AbstractBlock.Settings.create().mapColor(DyeColor.MAGENTA).instrument(NoteBlockInstrument.SNARE).strength(0.5F).sounds(BlockSoundGroup.SAND)
+            ));
+    public static final Block SPRING_GREEN_CONCRETE = registerBlock(
+            "spring_green_concrete", new Block(AbstractBlock.Settings.create().mapColor(DyeColor.CYAN).instrument(NoteBlockInstrument.BASEDRUM).requiresTool().strength(1.8F))
+    );
+    public static final Block SPRING_GREEN_CONCRETE_POWDER = registerBlock("spring_green_concrete_powder",
+            new ConcretePowderBlock(
+                    SPRING_GREEN_CONCRETE, AbstractBlock.Settings.create().mapColor(DyeColor.CYAN).instrument(NoteBlockInstrument.SNARE).strength(0.5F).sounds(BlockSoundGroup.SAND)
+            ));
+    public static final Block ULTRAMARINE_CONCRETE = registerBlock(
+            "ultramarine_concrete", new Block(AbstractBlock.Settings.create().mapColor(DyeColor.LIGHT_BLUE).instrument(NoteBlockInstrument.BASEDRUM).requiresTool().strength(1.8F))
+    );
+    public static final Block ULTRAMARINE_CONCRETE_POWDER = registerBlock("ultramarine_concrete_powder",
+            new ConcretePowderBlock(
+                    ULTRAMARINE_CONCRETE, AbstractBlock.Settings.create().mapColor(DyeColor.LIGHT_BLUE).instrument(NoteBlockInstrument.SNARE).strength(0.5F).sounds(BlockSoundGroup.SAND)
+            ));
+    public static final Block CHARTREUSE_TERRACOTTA = registerBlock(
+            "chartreuse_terracotta",
+            new Block(AbstractBlock.Settings.create().mapColor(MapColor.TERRACOTTA_LIME).instrument(NoteBlockInstrument.BASEDRUM).requiresTool().strength(1.25F, 4.2F))
+    );
+    public static final Block ROSE_TERRACOTTA = registerBlock(
+            "rose_terracotta",
+            new Block(AbstractBlock.Settings.create().mapColor(MapColor.TERRACOTTA_MAGENTA).instrument(NoteBlockInstrument.BASEDRUM).requiresTool().strength(1.25F, 4.2F))
+    );
+    public static final Block SPRING_GREEN_TERRACOTTA = registerBlock(
+            "spring_green_terracotta",
+            new Block(AbstractBlock.Settings.create().mapColor(MapColor.TERRACOTTA_CYAN).instrument(NoteBlockInstrument.BASEDRUM).requiresTool().strength(1.25F, 4.2F))
+    );
+    public static final Block ULTRAMARINE_TERRACOTTA = registerBlock(
+            "ultramarine_terracotta",
+            new Block(AbstractBlock.Settings.create().mapColor(MapColor.TERRACOTTA_LIGHT_BLUE).instrument(NoteBlockInstrument.BASEDRUM).requiresTool().strength(1.25F, 4.2F))
+    );
+    public static final Block CHARTREUSE_GLAZED_TERRACOTTA = registerBlock(
+            "chartreuse_glazed_terracotta",
+            new GlazedTerracottaBlock(
+                    AbstractBlock.Settings.create()
+                            .mapColor(DyeColor.LIME)
+                            .instrument(NoteBlockInstrument.BASEDRUM)
+                            .requiresTool()
+                            .strength(1.4F)
+                            .pistonBehavior(PistonBehavior.PUSH_ONLY)
+
+            )
+    );
+    public static final Block ROSE_GLAZED_TERRACOTTA = registerBlock(
+            "rose_glazed_terracotta",
+            new GlazedTerracottaBlock(
+                    AbstractBlock.Settings.create()
+                            .mapColor(DyeColor.MAGENTA)
+                            .instrument(NoteBlockInstrument.BASEDRUM)
+                            .requiresTool()
+                            .strength(1.4F)
+                            .pistonBehavior(PistonBehavior.PUSH_ONLY)
+            )
+    );
+    public static final Block SPRING_GREEN_GLAZED_TERRACOTTA = registerBlock(
+            "spring_green_glazed_terracotta",
+            new GlazedTerracottaBlock(
+                    AbstractBlock.Settings.create()
+                            .mapColor(DyeColor.CYAN)
+                            .instrument(NoteBlockInstrument.BASEDRUM)
+                            .requiresTool()
+                            .strength(1.4F)
+                            .pistonBehavior(PistonBehavior.PUSH_ONLY)
+            )
+    );
+    public static final Block ULTRAMARINE_GLAZED_TERRACOTTA = registerBlock(
+            "ultramarine_glazed_terracotta",
+            new GlazedTerracottaBlock(
+                    AbstractBlock.Settings.create()
+                            .mapColor(DyeColor.LIGHT_BLUE)
+                            .instrument(NoteBlockInstrument.BASEDRUM)
+                            .requiresTool()
+                            .strength(1.4F)
+                            .pistonBehavior(PistonBehavior.PUSH_ONLY)
+            )
+    );
+    public static final Block CHARTREUSE_WOOL = registerBlock(
+            "chartreuse_wool",
+            new Block(
+                    AbstractBlock.Settings.create().mapColor(MapColor.LIME).instrument(NoteBlockInstrument.GUITAR).strength(0.8F).sounds(BlockSoundGroup.WOOL).burnable()
+            )
+    );
+    public static final Block ROSE_WOOL = registerBlock(
+            "rose_wool",
+            new Block(
+                    AbstractBlock.Settings.create().mapColor(MapColor.MAGENTA).instrument(NoteBlockInstrument.GUITAR).strength(0.8F).sounds(BlockSoundGroup.WOOL).burnable()
+            )
+    );
+    public static final Block SPRING_GREEN_WOOL = registerBlock(
+            "spring_green_wool",
+            new Block(
+                    AbstractBlock.Settings.create().mapColor(MapColor.CYAN).instrument(NoteBlockInstrument.GUITAR).strength(0.8F).sounds(BlockSoundGroup.WOOL).burnable()
+            )
+    );
+    public static final Block ULTRAMARINE_WOOL = registerBlock(
+            "ultramarine_wool",
+            new Block(
+                    AbstractBlock.Settings.create().mapColor(MapColor.LIGHT_BLUE).instrument(NoteBlockInstrument.GUITAR).strength(0.8F).sounds(BlockSoundGroup.WOOL).burnable()
+            )
+    );
+    public static final Block CHARTREUSE_STAINED_GLASS = registerBlock("chartreuse_stained_glass", new StainedGlassBlock(DyeColor.BLUE,
+            AbstractBlock.Settings.create()
+                    .instrument(NoteBlockInstrument.HAT).strength(0.3F).sounds(BlockSoundGroup.GLASS).nonOpaque()
+    ));
+    public static final Block CHARTREUSE_STAINED_GLASS_PANE = registerBlock(
+            "chartreuse_stained_glass_pane",
+            new StainedGlassPaneBlock(
+                    DyeColor.BLUE, AbstractBlock.Settings.create().instrument(NoteBlockInstrument.HAT).strength(0.3F).sounds(BlockSoundGroup.GLASS).nonOpaque()
+            )
+    );
+
+    private static Block registerBlock(String name, Block block) {
+        registerBlockItem(name, block);
+        return Registry.register(Registries.BLOCK, Identifier.of(AncientDyes.MOD_ID, name), block);
+    }
+
+    private static void registerBlockItem(String name, Block block) {
+        Registry.register(Registries.ITEM, Identifier.of(AncientDyes.MOD_ID, name),
+        new BlockItem(block, new Item.Settings()));
+
+    }
+
+    public static void registerModBlocks(){
+        AncientDyes.LOGGER.info("Registering Mod Blocks for " + AncientDyes.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.COLORED_BLOCKS).register(fabricItemGroupEntries -> {
+            fabricItemGroupEntries.add(ModBlocks.CHARTREUSE_WOOL);
+            fabricItemGroupEntries.add(ModBlocks.CHARTREUSE_CONCRETE);
+            fabricItemGroupEntries.add(ModBlocks.CHARTREUSE_CONCRETE_POWDER);
+            fabricItemGroupEntries.add(ModBlocks.CHARTREUSE_TERRACOTTA);
+            fabricItemGroupEntries.add(ModBlocks.CHARTREUSE_GLAZED_TERRACOTTA);
+            fabricItemGroupEntries.add(ModBlocks.CHARTREUSE_STAINED_GLASS);
+            fabricItemGroupEntries.add(ModBlocks.CHARTREUSE_STAINED_GLASS_PANE);
+            fabricItemGroupEntries.add(ModBlocks.ROSE_WOOL);
+            fabricItemGroupEntries.add(ModBlocks.ROSE_CONCRETE);
+            fabricItemGroupEntries.add(ModBlocks.ROSE_CONCRETE_POWDER);
+            fabricItemGroupEntries.add(ModBlocks.ROSE_TERRACOTTA);
+            fabricItemGroupEntries.add(ModBlocks.ROSE_GLAZED_TERRACOTTA);
+            fabricItemGroupEntries.add(ModBlocks.SPRING_GREEN_WOOL);
+            fabricItemGroupEntries.add(ModBlocks.SPRING_GREEN_CONCRETE);
+            fabricItemGroupEntries.add(ModBlocks.SPRING_GREEN_CONCRETE_POWDER);
+            fabricItemGroupEntries.add(ModBlocks.SPRING_GREEN_TERRACOTTA);
+            fabricItemGroupEntries.add(ModBlocks.SPRING_GREEN_GLAZED_TERRACOTTA);
+            fabricItemGroupEntries.add(ModBlocks.ULTRAMARINE_WOOL);
+            fabricItemGroupEntries.add(ModBlocks.ULTRAMARINE_CONCRETE);
+            fabricItemGroupEntries.add(ModBlocks.ULTRAMARINE_CONCRETE_POWDER);
+            fabricItemGroupEntries.add(ModBlocks.ULTRAMARINE_TERRACOTTA);
+            fabricItemGroupEntries.add(ModBlocks.ULTRAMARINE_GLAZED_TERRACOTTA);
+        });
+    }
+}
---
+++ src/main/java/net/skrubs/ancientdyes/item/ModItems.java
@@ -0,0 +1,46 @@
+package net.skrubs.ancientdyes.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.DyeItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.DyeColor;
+import net.minecraft.util.Identifier;
+import net.skrubs.ancientdyes.AncientDyes;
+
+public class ModItems {
+    public static final Item CHARTREUSE_DYE = registerItem("chartreuse_dye", new Item(new Item.Settings()));
+    public static final Item ROSE_DYE = registerItem("rose_dye", new Item(new Item.Settings()));
+    public static final Item SPRING_GREEN_DYE = registerItem("spring_green_dye", new Item(new Item.Settings()));
+    public static final Item ULTRAMARINE_DYE = registerItem("ultramarine_dye", new Item(new Item.Settings()));
+    public static final Item CYAN_FLOWER_SEEDS = registerItem("cyan_flower_seeds", new Item(new Item.Settings()));
+    public static final Item DANDELION_SEEDS = registerItem("dandelion_seeds", new Item(new Item.Settings()));
+    public static final Item FLOWER_SEEDS = registerItem("flower_seeds", new Item(new Item.Settings()));
+    public static final Item ROSE_SEEDS = registerItem("rose_seeds", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(AncientDyes.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        AncientDyes.LOGGER.info("Registering Mod Items for " + AncientDyes.MOD_ID);
+
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(fabricItemGroupEntries -> {
+            fabricItemGroupEntries.add(CHARTREUSE_DYE);
+            fabricItemGroupEntries.add(ROSE_DYE);
+            fabricItemGroupEntries.add(SPRING_GREEN_DYE);
+            fabricItemGroupEntries.add(ULTRAMARINE_DYE);
+        });
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.NATURAL).register(fabricItemGroupEntries -> {
+            fabricItemGroupEntries.add(CYAN_FLOWER_SEEDS);
+            fabricItemGroupEntries.add(ROSE_SEEDS);
+            fabricItemGroupEntries.add(DANDELION_SEEDS);
+            fabricItemGroupEntries.add(FLOWER_SEEDS);
+
+        });
+    }
+}
